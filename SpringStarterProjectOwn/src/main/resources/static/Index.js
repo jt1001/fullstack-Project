1c0f9b31c82623670a4c5c7d8610a1ac8c0f5a2f
jt1001 <jovanlana@hotmail.co.uk>
2022-03-03 16:59:21 +0000

    allcars function working

--- SpringStarterProjectOwn/src/main/resources/static/Index.js
+++ SpringStarterProjectOwn/src/main/resources/static/Index.js
@@ -3,13 +3,31 @@ function addcarfield() {
 }
 
 
-let allCars = () => fetch('http://localhost:8080/getAll')
-.then ((resonse) => {
-    if (resonse.status !== 200) {
-        console.error('status: ${response.status}');
-        return;
-    }
-    resonse.json()
-    .then(data => { console.log(data.data) ;return data.json})
-}) .catch((err) =>console.log('${err'));
+const allCars = async () => { //async allows this function to return a promise!!!
+  let response = await fetch('http://localhost:8080/getAll');
+  if (response.status !== 200) {
+      throw new Error("Request has failed!");
+
+  }
+  console.log("Request Successful");
+  let jsonData = await response.json();
+  console.log(jsonData);
+  return jsonData.data;
+}
+
 allCars();
+
+// let showAllCars = async () => {
+//   let returnedData = await allCars();
+//   paragraphToSelect.innerHTML = "";
+//   //Ita through the json data
+//   for (let d of returnedData) {
+//           let div = document.createElement("div");
+//           //adding styling to each div 
+//           div.style = "margin:10px;"
+//           //matching the object syntax given in console.log(data)
+//           div.innerHTML = `Firstname: ${d.first_name}, Lastname: ${d.last_name}, Email: ${d.email}`;
+//           //appending our para -> div
+//           paragraphToSelect.append(div);
+//       }
+//   }
