400895d8c12dfa175e7293ed0a3e3708c9242548
jt1001 <jovanlana@hotmail.co.uk>
2022-03-04 12:47:45 +0000

    part 1 of returning all car data

--- SpringStarterProjectOwn/src/main/resources/static/Index.js
+++ SpringStarterProjectOwn/src/main/resources/static/Index.js
@@ -1,33 +1,21 @@
-function addcarfield() {
-  alert("connect me to add new car :(");
-}
+"use strict";
 
-
-const allCars = async () => { //async allows this function to return a promise!!!
-  let response = await fetch('http://localhost:8080/getAll');
-  if (response.status !== 200) {
-      throw new Error("Request has failed!");
-
-  }
-  console.log("Request Successful");
-  let jsonData = await response.json();
-  console.log(jsonData);
-  return jsonData.data;
-}
-
-allCars();
-
-// let showAllCars = async () => {
-//   let returnedData = await allCars();
-//   paragraphToSelect.innerHTML = "";
-//   //Ita through the json data
-//   for (let d of returnedData) {
-//           let div = document.createElement("div");
-//           //adding styling to each div 
-//           div.style = "margin:10px;"
-//           //matching the object syntax given in console.log(data)
-//           div.innerHTML = `Firstname: ${d.first_name}, Lastname: ${d.last_name}, Email: ${d.email}`;
-//           //appending our para -> div
-//           paragraphToSelect.append(div);
-//       }
-//   }
+let allcars = () => {
+  fetch(`http://localhost:8080/getAll`)
+    .then((response) => {
+      if (response.status !== 200) {
+        console.error(`status: ${reponse.status}`);
+        return;
+      }
+      response.json().then((data) => console.info(data));
+    })
+    .catch((err) => console.error(`${err}`));
+};
+// function appendData(data) {
+//     var mainContainer = document.getElementById("allcarsData");
+//     for (var i = 0; i < data.length; i++) {
+//         var div = document.createElement("div");
+//         div.innerHTML = 'ID: ' + data[i].id + 'Car Make' + data[i].carMake;
+//         mainContainer.appendChild(div);
+//     }
+// }
