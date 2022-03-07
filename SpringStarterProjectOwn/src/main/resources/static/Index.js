3c710460213004d4149f0dbe24014c1369707f7e
jt1001 <jovanlana@hotmail.co.uk>
2022-03-07 11:38:32 +0000

    updated show forms

--- SpringStarterProjectOwn/src/main/resources/static/Index.js
+++ SpringStarterProjectOwn/src/main/resources/static/Index.js
@@ -1,78 +1,95 @@
 "use strict";
 
-function allcars(){ //working and shows data on index
-    let refPromise=fetch(`http://localhost:8080/getAll`)
-    refPromise.then(
-        function(response) {
-            let refResponsePromise=response.json()
-            refResponsePromise.then(function(data){
-
-                for(let i=0;i<data.length;i++){
-
-                    let refDiv=document.createElement("div")
-                    refDiv.innerHTML="ID: "+ data[i].id + "Car Make: "+data[i].carMake + "Model: " + data[i].modelName + "Year: " + data[i].makeYear + "Engine Size: " + data[i].engineSize + "Price: " + data[i].price
-                    document.body.appendChild(refDiv)
-                }
-            })
-        }
-    )
+function addcarfield() {
+  if (addform.value == "Add a new Car") {
+    carForm.style.visibility = "visible";
+    addform.value = "Hide Form";
+  } else {
+    carForm.style.visibility = "hidden";
+    addform.value = "Add a new Car";
+  }
 }
 
-// let allcars = () => {
-//   fetch(`http://localhost:8080/getAll`)
-//     .then((response) => {
-//       if (response.status !== 200) {
-//         console.error(`status: ${response.status}`);
-//         return;
-//       }
-//       response.json().then((data) => console.info(data));
-//     })
-//     .catch((err) => console.error(`${err}`));
-// };
+function showCars(id, carMake, modelName, makeYear, engineSize, price) {
+  let refRow = document.createElement("tr");
+  let refTd1 = document.createElement("td");
+  let refTd2 = document.createElement("td");
+  let refTd3 = document.createElement("td");
+  let refTd4 = document.createElement("td");
+  let refTd5 = document.createElement("td");
+  let refTd6 = document.createElement("td");
 
-// var mainContainer = document.getElementById("allcarsData");
+  refTd1.innerHTML = id;
+  refTd2.innerHTML = carMake;
+  refTd3.innerHTML = modelName;
+  refTd4.innerHTML = makeYear;
+  refTd5.innerHTML = engineSize;
+  refTd6.innerHTML = price;
 
-// function viewAllCars() {
-//     let data = allcars();
-//         for (let d in data) {
-//       let div = document.createElement("div");
-//       div.innerHTML = `ID: ${d.id}, carMake: ${d.carMake}, modelName: ${d.modelName}, makeYear: ${d.makeYear}, engineSize: ${d.engineSize}, price: ${d.price}`;
-//       mainContainer.appendChild(div);
-//     }
-//   }
+  refRow.appendChild(refTd1);
+  refRow.appendChild(refTd2);
+  refRow.appendChild(refTd3);
+  refRow.appendChild(refTd4);
+  refRow.appendChild(refTd5);
+  refRow.appendChild(refTd6);
 
-// function viewAllCars() {
-//   let refTable= document.createElement("table");
+  refTable.appendChild(refRow);
+  document.body.appendChild(refTable);
+}
+
+function createCar() {
   
-//   let data = allcars();
-//   for (let i=0;i<data.length;i++) {
-//     let refRow=document.createElement("tr")
-//     let refTd1=document.createElement("td")
-//     let refTd2=document.createElement("td")
-//     let refTd3=document.createElement("td")
-//     let refTd4=document.createElement("td")
-//     let refTd5=document.createElement("td")
-//     let refTd6=document.createElement("td")
+}
 
-//     refTd1.innerHTML=data[i].id
-//     refTd2.innerHTML=data[i].carMake
-//     refTd1.innerHTML=data[i].modelName
-//     refTd2.innerHTML=data[i].makeYear
-//     refTd1.innerHTML=data[i].engineSize
-//     refTd2.innerHTML=data[i].price
+function viewAllCarsButton() { //NEEDS FIXING
+  if (viewCarsBtn.value == "View all cars in stock") {
+    carForm.style.visibility = "visible";
+    addform.value = "Hide Form";
+  } else {
+    carForm.style.visibility = "hidden";
+    addform.value = "Add a new Car";
+  }
+}
 
-//     refRow.appendChild(refTd1);
-//     refRow.appendChild(refTd2);
-//     refRow.appendChild(refTd3);
-//     refRow.appendChild(refTd4);
-//     refRow.appendChild(refTd5);
-//     refRow.appendChild(refTd6);
+let refTable = "";
+function allcars() {
+  //working and shows data on index
+  let refPromise = fetch(`http://localhost:8080/getAll`);
+  refPromise.then(function (response) {
+    let refResponsePromise = response.json();
+    refResponsePromise.then(function (data) {
+      refTable = document.createElement("table");
+      refTable.border = 10;
 
-//     refTable.appendChild(refRow)
-//   }
-//   document.body.appendChild(refTable)
+      for (let i = 0; i < data.length; i++) {
+        showCars(
+          data[i].id,
+          data[i].carMake,
+          data[i].modelName,
+          data[i].makeYear,
+          data[i].engineSize,
+          data[i].price
+        );
+      }
+    });
+  });
+}
+
+// function allcars(){ //working and shows data on index
+//     let refPromise=fetch(`http://localhost:8080/getAll`)
+//     refPromise.then(
+//         function(response) {
+//             let refResponsePromise=response.json()
+//             refResponsePromise.then(function(data){
+
+//                 for(let i=0;i<data.length;i++){
+//                     document.createElement
+
+//                     let refDiv=document.createElement("div")
+//                     refDiv.innerHTML="ID: "+ data[i].id + "Car Make: "+data[i].carMake + "Model: " + data[i].modelName + "Year: " + data[i].makeYear + "Engine Size: " + data[i].engineSize + "Price: " + data[i].price
+//                     document.body.appendChild(refDiv)
+//                 }
+//             })
+//         }
+//     )
 // }
-
-
-
-
